1b7c7f102a8d21a5a2421550a1943f014cdf3bda
hapynez <highesthap@gmail.com>
2023-07-20 15:43:00 +0100

    added a component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-
 import "./App.css";
-import axios from "axios";
+import ReactAnimatedWeather from "react-animated-weather";
+import SearchEngine from "./SearchEngine";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>hello nputut43</h1>
+        <h3>
+          <ul className="navigation-items">
+            <li class="navigation-item">
+              <a href="/">Lisbon</a>
+            </li>
+            <li class="navigation-item">
+              <a href="/paris">Paris</a>
+            </li>
+            <li class="navigation-item">
+              <a href="/sydney">Sydney</a>
+            </li>
+            <li class="navigation-item">
+              <a href="/san-francisco">San Francisco</a>
+            </li>
+          </ul>
+        </h3>
+        <SearchEngine />
+        <div class="weather-summary-header">
+          <h1>Lagos</h1>
+          <div class="weather-detail__text">Thursday 12:38</div>
+          <div class="weather-detail__text">Rain</div>
+        </div>
+
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="clearfix">
+              <div class="weather-temp weather-temp--today">29</div>
+              <div class="weather-unit__text weather-unit__text--today">°C</div>
+              <ReactAnimatedWeather
+                icon="SNOW"
+                color="white"
+                size="49"
+                animate={true}
+              />
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="weather-detail__text">Precipitation: 74%</div>
+            <div class="weather-detail__text">Wind: 4km/h</div>
+          </div>
+        </div>
       </header>
     </div>
   );
---
+++ src/SearchEngine.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+export default function SearchEngine () {
+  
+    return (
+      <form className="OnSubmit" >
+        <input type="search" placeholder="Type a city"/>
+        <input type="submit" value="Search" />
+      </form>
+    );
+}
